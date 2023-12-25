c4669a19e332b24ab156e33ea554f33dab94d035
moein-Ahmadi <moeinahmadi57@gmail.com>
2023-12-25 20:39:34 +0330

    page1

---
+++ src/java.js
@@ -0,0 +1,13 @@
+// header
+
+const myFunction = () => {
+  let x = document.getElementById("myDiv");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+};
+
+// header
+// ----------------------------
